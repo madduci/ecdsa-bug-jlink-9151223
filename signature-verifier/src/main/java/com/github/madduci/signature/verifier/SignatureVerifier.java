f4fe6412572f695cc2afd9535493415c53f09af5
Michele Adduci <michele.adduci@openlimit.com>
2020-05-12 09:05:05 +0200

    Added generation of jlink + jpackage

--- signature-verifier/src/main/java/com/github/madduci/signature/verifier/SignatureVerifier.java
+++ signature-verifier/src/main/java/com/github/madduci/signature/verifier/SignatureVerifier.java
@@ -14,7 +14,6 @@ import java.security.InvalidKeyException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
---
+++ signature-verifier/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author m_adduci
+ * created on May 12, 2020
+ */
+module signature.verifier {
+  requires argparse4j;
+  exports com.github.madduci.signature.verifier;
+}
